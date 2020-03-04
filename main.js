5c52e7a854b4e73f911a8cc2f727b5fd8cabed24
e-magon <ahabhyde@gmail.com>
2020-03-04 14:03:48 +0100

    Previous window size is restored when starting the app

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const { app, BrowserWindow, screen, dialog, shell } = require("electron");
 
-function startup() {
+async function startup() {
   let { width, height } = screen.getPrimaryDisplay().workAreaSize;
   width = parseInt(0.85 * width);
   height = parseInt(0.85 * height);
@@ -9,6 +9,7 @@ function startup() {
     width: width,
     height: height,
     backgroundColor: "#000000",
+    show: false,
     webPreferences: {
       devTools: false
     }
@@ -33,13 +34,34 @@ function startup() {
     }
   });
 
-  appWindow.webContents.on('new-window', function (e, url) {
+  appWindow.webContents.on("new-window", function (e, url) {
     //Opens external urls in the system browser
     e.preventDefault();
     shell.openExternal(url);
   });
 
+  let resizeTimer;
+  appWindow.on("resize", () => {
+    let [width, height] = appWindow.getSize();
+
+    //This timer saves the current window size only when it has been the same for 200 ms
+    clearTimeout(resizeTimer);
+    resizeTimer = setTimeout(async () => {
+      appWindow.webContents.executeJavaScript(`localStorage.setItem("lastWidth", ${width})`);
+      appWindow.webContents.executeJavaScript(`localStorage.setItem("lastHeight", ${height})`);
+    }, 200);
+  });
+
   appWindow.loadFile("assets/CiarnuroTimer-Web/index.html");
+
+  let lastWidth = parseInt(await appWindow.webContents.executeJavaScript(`localStorage.getItem("lastWidth")`));
+  let lastHeight = parseInt(await appWindow.webContents.executeJavaScript(`localStorage.getItem("lastHeight")`));
+
+  if (!isNaN(lastWidth) && !isNaN(lastHeight))
+    appWindow.setSize(lastWidth, lastHeight, false);
+
+  appWindow.center();
+  setTimeout(() => { appWindow.show(); }, 150); //The timeout prevents showing the page resizing its content
 }
 
 app.whenReady().then(startup);
